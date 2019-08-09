77775b35fbdb4f4522916432ff7128ee69882213
ArshadMajgaonkar <53622937+ArshadMajgaonkar@users.noreply.github.com>
2019-08-09 23:12:36 +0530

    upload1

---
+++ Calc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Calc
+{
+    public static void main(String arg[]) {
+        Scanner obj=new Scanner(System.in);
+        int a,b,ch;
+
+        System.out.println("enter 2 integers:");
+        a-nextInt();
+        b-nextInt();
+    }
+}
